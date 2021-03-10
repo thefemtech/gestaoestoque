748271a79e30e719f12d16455e7c32fd5ff8b27a
matheus <matheus.nita78@gmail.com>
2021-03-10 14:53:22 -0300

    Um sistema de estoque esgotado foi adicionado

--- src/main/java/gestaoestoque/thefemtech/com/br/controller/EstoqueController.java
+++ src/main/java/gestaoestoque/thefemtech/com/br/controller/EstoqueController.java
@@ -1,13 +1,17 @@
 package gestaoestoque.thefemtech.com.br.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.bind.annotation.MatrixVariable;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import gestaoestoque.thefemtech.com.br.model.Jogo;
 import gestaoestoque.thefemtech.com.br.repository.JogoRepository;
 
@@ -37,4 +41,31 @@ public class EstoqueController {
 	}
 	
 
+	public ResponseEntity<?> consultarId (@PathVariable Long id) {
+		try {
+			Optional<Jogo> jogo = jogoRepository.findById(id);
+			if(!jogo.isPresent()) {
+				return ResponseEntity.status(418).build();
+			}
+			return ResponseEntity.ok(jogo);
+		}catch(Exception ex) {
+			return ResponseEntity.badRequest().build();
+		}
+	}
+	
+
+	public ResponseEntity<?> deletarJogo(@PathVariable Long id) {
+		try {
+			jogoRepository.deleteById(id);
+			return ResponseEntity.ok().build();
+		}catch(Exception ex) {
+			return ResponseEntity.badRequest().build();
+		}
+	}
+	
+
+	public String getIdMatrixVariable(@MatrixVariable("id") String id) {
+	    return "ID: " + id;
+	}
+	
 }
